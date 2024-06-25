5a38dbccc631cbda4c6d1e8254241369d5b6f2d5
Jackson James <jackson.james@converj.com>
2024-06-25 21:15:04 +0530

    after design

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import "./App.css";
 import JobList from "./JobPage";
 
-import { Typography, Grid } from "@mui/material";
-
 function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<Typography>Job App</Typography>
-				<Grid
-					sx={{ display: "flex", flexDirection: "column", background: "gray" }}
-				>
-					<JobList />
-				</Grid>
+				<JobList />
 			</header>
 		</div>
 	);
--- src/JobPage.js
+++ src/JobPage.js
@@ -1,6 +1,14 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { TextField, Grid, Button } from "@mui/material";
+import {
+	Grid,
+	Button,
+	Typography,
+	Paper,
+	InputBase,
+	Divider,
+	CircularProgress,
+} from "@mui/material";
 
 function JobList() {
 	const [jobs, setJobs] = useState([]);
@@ -35,12 +43,27 @@ function JobList() {
 			console.error("Error submitting job:", error);
 		}
 	};
+	const handleReset = async () => {
+		//resets all jobs in backend
+		try {
+			await axios.delete("http://localhost:8080/jobs");
+			setName("");
+			setDuration("");
+		} catch (error) {
+			console.error("Error resetting jobs:", error);
+		} finally {
+			setJobs([]);
+		}
+	};
 
 	useEffect(() => {
-		const ws = new WebSocket("ws://localhost:8080/ws");
+		const ws = new WebSocket("ws://localhost:8080/ws"); //connecting to ws
+
 		ws.onmessage = (event) => {
+			//listening to messages
 			const newJob = JSON.parse(event.data);
 			setJobs((prevJobs) => {
+				//updating existing state based on the recieved message (using unique id to check which job got updated)
 				const jobIndex = prevJobs.findIndex((job) => job.id === newJob.id);
 				if (jobIndex > -1) {
 					return prevJobs.map((job) =>
@@ -57,48 +80,178 @@ function JobList() {
 	}, []);
 
 	return (
-		<div>
-			{jobs?.map((job) => (
-				<div key={job.id} style={{ color: getColorByStatus(job.status) }}>
-					{job.name} - {job.duration}s - {job.status}
-				</div>
-			))}
-			<form onSubmit={handleSubmit}>
-				<Grid
+		<Grid>
+			<Typography fontSize={30} fontWeight={600} paddingBottom={5}>
+				Shortest Job First scheduling - Apica assignment
+			</Typography>
+			<Grid sx={{ display: "flex", columnGap: 10 }}>
+				<Grid // left section shows the available tasks and its status
 					sx={{
-						display: "flex",
-						flexDirection: "column",
-						marginTop: 10,
-						rowGap: 2,
+						minWidth: 500,
+						background: "#1B1A1D",
+						padding: 3,
+						minHeight: 300,
+						overflowY: "auto",
+						maxHeight: "50vh",
+						borderRadius: 5,
+						boxShadow: "0px 4px 18px rgba(0, 0, 0, 0.5)",
 					}}
 				>
-					<TextField
-						id="outlined-basic"
-						label="Job Name"
-						variant="outlined"
-						onChange={(e) => setName(e.target.value)}
-						value={name}
-					/>
-					<TextField
-						id="outlined-basic"
-						label="Duration (s)"
-						variant="outlined"
-						onChange={(e) => setDuration(e.target.value)}
-						value={duration}
-					/>
-					<Button variant="contained" type="submit">
-						Submit Job
+					{jobs?.map(
+						(
+							job // maps the response to tiles
+						) => (
+							<Grid
+								key={job.id}
+								sx={{
+									background: "#484859",
+									marginBottom: 1,
+									borderRadius: 2,
+									display: "flex",
+									justifyContent: "space-around",
+									alignItems: "center",
+								}}
+							>
+								<Typography fontSize={25} width={"20%"}>
+									{job.name}
+								</Typography>
+								<Typography fontSize={25} width={"20%"}>
+									{job.duration / 1e9}s
+								</Typography>
+								<Grid
+									sx={{
+										background: getColorByStatus(job.status),
+										paddingX: 2,
+										display: "flex",
+										alignItems: "center",
+										borderRadius: 2,
+									}}
+									width={"23%"}
+								>
+									<Typography sx={{ color: "white" }}>{job.status}</Typography>
+								</Grid>
+								{job.status === "running" ? ( // if currently running circular progress
+									<CircularProgress color="primary" size={20} width={"20%"} />
+								) : null}
+							</Grid>
+						)
+					)}
+				</Grid>
+				<Grid //right section give a form to add new jobs
+					sx={{
+						minWidth: 500,
+						minHeight: 300,
+						background: "#1B1A1D",
+						padding: 7,
+						borderRadius: 5,
+						boxShadow: "0px 4px 18px rgba(0, 0, 0, 0.2)",
+					}}
+				>
+					<form onSubmit={handleSubmit}>
+						<Grid
+							sx={{
+								display: "flex",
+								flexDirection: "column",
+								marginTop: 10,
+								rowGap: 2,
+								alignItems: "center",
+							}}
+						>
+							<Paper
+								component="form"
+								sx={{
+									p: "2px 4px",
+									display: "flex",
+									alignItems: "center",
+									width: 400,
+									background: "#abc4b2",
+								}}
+							>
+								<InputBase
+									placeholder="Job name"
+									inputProps={{ "aria-label": "search google maps" }}
+									id="outlined-basic"
+									label="Job Name"
+									variant="outlined"
+									onChange={(e) => setName(e.target.value)}
+									value={name}
+									sx={{
+										"& .MuiOutlinedInput-root": { borderColor: "white" },
+										ml: 1,
+										flex: 1,
+									}}
+								/>
+							</Paper>
+							<Paper
+								component="form"
+								sx={{
+									p: "2px 4px",
+									display: "flex",
+									alignItems: "center",
+									width: 400,
+									background: "#abc4b2",
+								}}
+							>
+								<InputBase
+									placeholder="duration in seconds (number only)"
+									inputProps={{ "aria-label": "search google maps" }}
+									id="outlined-basic"
+									label="Duration (s)"
+									variant="outlined"
+									onChange={(e) => setDuration(e.target.value)}
+									value={duration}
+									sx={{
+										"& .MuiOutlinedInput-root": { borderColor: "white" },
+										ml: 1,
+										flex: 1,
+									}}
+								/>
+
+								<Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+							</Paper>
+
+							<Button
+								variant="contained"
+								type="submit"
+								sx={{
+									"&.Mui-disabled": {
+										background: "gray",
+										color: "white",
+									},
+								}}
+								disabled={
+									name === "" || duration === "" || isNaN(parseInt(duration)) //should fill both fields and duration in number
+								}
+							>
+								Submit Job
+							</Button>
+						</Grid>
+					</form>
+					<Button
+						variant="contained"
+						onClick={handleReset}
+						sx={{
+							"&.Mui-disabled": {
+								background: "gray",
+								color: "white",
+							},
+						}}
+						disabled={
+							jobs.length === 0 //should fill both fields and duration in number
+						}
+					>
+						Reset
 					</Button>
 				</Grid>
-			</form>
-		</div>
+			</Grid>
+		</Grid>
 	);
 }
 
 function getColorByStatus(status) {
 	switch (status) {
 		case "pending":
-			return "blue";
+			return "red";
 		case "running":
 			return "blue";
 		case "completed":
